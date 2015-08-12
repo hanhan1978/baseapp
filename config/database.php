bdb00191c38c4905908ad6ff186769232b9fb808
hanhan1978 <ryo.tomidokoro@gmail.com>
2015-08-12 20:51:19 +0900

    added migration

--- config/database.php
+++ config/database.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -55,9 +55,9 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', ''),
+            'database'  => env('DB_DATABASE', 'baseapp'),
+            'username'  => env('DB_USERNAME', 'develop'),
+            'password'  => env('DB_PASSWORD', 'develop'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
---
+++ database/migrations/2015_08_11_132945_create_todo_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTodoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('todos', function($table){
+        $table->increments('id');
+        $table->integer('usr_id');
+        $table->text('content');
+        $table->timestamps();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      Schema::drop('todos'); 
+    }
+}
